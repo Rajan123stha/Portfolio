1b7cfab4cadba7d03f6d81af4af92d868e71fe89
Rajan123stha <rajan1234stha@gmail.com>
2023-05-03 18:58:56 +0545

    making responsive

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,3 +1,10 @@
+/*menu icon navbar */
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
 /*scroll section active link*/
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a');
@@ -20,4 +27,7 @@ window.onscroll = () => {
     /*sticky navbar*/
     let header = document.querySelector('.header');
     header.classList.toggle('sticky', window.scrollY > 100);
+    /*remove menu icon navbar when click navbar link(scroll)*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
