040b37bb0cb064983b156133fd236c2a9bea9946
Rajan123stha <rajan1234stha@gmail.com>
2023-05-10 17:35:03 +0545

    Added feedback and almost done

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,6 +1,6 @@
 /*menu icon navbar */
-let menuIcon=document.querySelector('#menu-icon');
-let navbar=document.querySelector('.navbar');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
@@ -21,13 +21,30 @@ window.onscroll = () => {
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         }
-        
+
     });
-  
-    /*sticky navbar*/
+
+    /* sticky navbar */
     let header = document.querySelector('.header');
     header.classList.toggle('sticky', window.scrollY > 100);
     /*remove menu icon navbar when click navbar link(scroll)*/
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
+/* Dark Mode */
+let darkModeIcon = document.querySelector('#darkMode');
+darkModeIcon.onclick=()=> {
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+};
+/* scroll reveal */
+ScrollReveal({
+    reset: true,
+    distance:'80px',
+    duration:2000,
+    delay:200
+});
+ScrollReveal().reveal('.home-content, .heading', { origin:'top' });
+ScrollReveal().reveal('.home-img img, .services-container, .project-box, .contact form', { origin:'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin:'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin:'right' });
